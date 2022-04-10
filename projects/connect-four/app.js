b65a1282f7820082bb46c8de2ced0ea1731736d3
edwardchaos <edward_888@hotmail.com>
2022-04-10 18:08:55 -0400

    waste

--- projects/connect-four/app.js
+++ projects/connect-four/app.js
@@ -2,7 +2,16 @@
 
 document.addEventListener("DOMContentLoaded",() => {
 
-    squares = document.querySelectorAll(".grid div");
+    let squares = document.querySelectorAll(".grid div");
+    let current_player = document.querySelector("#current-player");
+    let result = document.querySelector("#result");
 
+    squares.forEach((square)=>{
+        square.onclick = clickSquare;
+    });
+
+    function clickSquare(){
+        console.log("clicked square.");
+    }
 
 });
