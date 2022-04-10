5525f45deda9db231ac8e9ee341cd15e37938631
edwardchaos <edward_888@hotmail.com>
2022-04-10 19:08:20 -0400

    move css variables to grid class

---
+++ projects/2048/script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+import Grid from "./grid.js"
+
+const gameBoard = document.querySelector("#game-board");
+
+console.log("hi1");
+const grid = new Grid(gameBoard);
+console.log("h21");
