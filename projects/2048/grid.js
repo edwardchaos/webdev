fd426918435e137f7b1af5a83050462aaa339abe
edwardchaos <edward_888@hotmail.com>
2022-04-10 19:13:49 -0400

    Create cells in grid moduel

--- projects/2048/grid.js
+++ projects/2048/grid.js
@@ -10,5 +10,20 @@ export default class Grid{
         gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
         gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
 
+        createCellElements(gridElement);
     }
+}
+
+function createCellElements(gridElement){
+    const cells = [];
+
+    for(let i = 0; i < GRID_SIZE; ++i){
+        for(let j = 0; j < GRID_SIZE; ++j){
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cells.push(cell);
+            gridElement.append(cell);
+        }
+    }
+    return cells;
 }
