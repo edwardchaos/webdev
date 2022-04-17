cc5cf3051b166e808ea9c6a0085086a3459d6ed8
edwardchaos <edward_888@hotmail.com>
2022-04-17 15:14:15 -0400

    setup input

--- projects/2048/script.js
+++ projects/2048/script.js
@@ -9,3 +9,29 @@ const grid = new Grid(game_board);
 
 grid.getRandomEmptyCell().tile = new Tile(game_board);
 grid.getRandomEmptyCell().tile = new Tile(game_board);
+
+setupInput();
+
+function setupInput(){
+    window.addEventListener("keydown", keyPressed, {once: true});
+}
+
+function keyPressed(event){
+    switch(event.key){
+        case "ArrowUp":
+            console.log("up");
+            break;
+        case "ArrowDown":
+            console.log("down");
+            break;
+        case "ArrowLeft":
+            console.log("left");
+            break;
+        case "ArrowRight":
+            console.log("right");
+            break;
+        default:
+            break;
+    }
+
+}
